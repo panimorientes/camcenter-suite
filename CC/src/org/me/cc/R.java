bfd2aca6c69cc1f3f8b9fbb29688fe0c5c1e9c66
Splitfirex <Splitfirex@gmail.com>
2011-06-25 21:11:55 +0000

    cambiado el xml

--- CC/src/org/me/cc/R.java
+++ CC/src/org/me/cc/R.java
@@ -17,13 +17,13 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int Lista=0x7f06000a;
+        public static final int Lista=0x7f06000c;
         public static final int changes=0x7f060011;
         public static final int datosconex=0x7f06000b;
         public static final int host=0x7f060001;
         public static final int image1=0x7f060010;
         public static final int infoconex=0x7f060009;
-        public static final int notifi=0x7f06000c;
+        public static final int notifi=0x7f06000e;
         public static final int probar=0x7f060007;
         public static final int puertoh=0x7f060004;
         public static final int servidor=0x7f06000d;
@@ -33,8 +33,8 @@ public final class R {
         public static final int widget29=0x7f060003;
         public static final int widget31=0x7f060005;
         public static final int widget32=0x7f060006;
-        public static final int widget37=0x7f06000e;
         public static final int widget40=0x7f06000f;
+        public static final int widget49=0x7f06000a;
     }
     public static final class layout {
         public static final int conexion=0x7f030000;
