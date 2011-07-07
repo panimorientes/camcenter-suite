76736472046d4c3b7838c793bb4ae47313c6930a
Splitfirex <Splitfirex@gmail.com>
2011-07-07 02:03:24 +0000

    agregado el unico puerto

--- ccmc damon/src/Main.java
+++ ccmc damon/src/Main.java
@@ -7,8 +7,7 @@ public class Main {
     
     public static void main(String[] args) throws Exception {
 
-      int puerto = 2002;
-      int puerto2 = 2003;
+      int puerto = 8888;
       if (args.length > 0) {
           if (args[0].equals("-p")){
                 puerto = Integer.parseInt(args[1]);
@@ -17,19 +16,9 @@ public class Main {
       File file = new File("index.html");
       if(file.exists()){
           file.delete();
-       }
-        HashMap Alertas = new HashMap();
-         HashMap Camaras = new HashMap();
-      
+       }     
         file.createNewFile();
-        SimpleWebServer webserver = new SimpleWebServer(2004);
-       // webserver.start();
-        Server g = new Server(puerto,"index.html",Alertas, Camaras);
-        serverMovil m = new serverMovil(puerto2, Alertas, Camaras);
-        m.start();
-        g.start();
-
-      
+        SimpleWebServer webserver = new SimpleWebServer(puerto);
     }
 
 }
